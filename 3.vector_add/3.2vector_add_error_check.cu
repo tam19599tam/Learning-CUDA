7d6b32d9329ab4e02fbd0c84fa7dc804aa648fff
tam <tam19599tam@gmail.com>
2026-03-05 15:26:40 +0800

    Refactor vector addition kernel and error checks

--- 3.vector_add/3.2vector_add_error_check.cu
+++ 3.vector_add/3.2vector_add_error_check.cu
@@ -1,15 +1,15 @@
 /*
-====== 原向量加法 + 错误检测 ======
+====== 向量加法(基础版) + 错误检测 ======
 总结：
-让自己显得很专业，就和C++在创建头文件之前用： #pragma once  一样。高大上一些。
-
+加入错误检测显得很专业，就和C++在创建头文件之前用： #pragma once  一样。高大上一些。
 
 */
+
+
 
 #include <stdio.h>
 #include <cuda.h>
 #include <cuda_runtime.h>   
-
 
 
 
@@ -28,68 +28,79 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 
 
-__global__ void vector_add(float* gpu_x, float* gpu_y, float* gpu_z, int N)    // 核函数，传入3个指针数组。
+__global__ void vector_add(float* gpu_x, float* gpu_y, float* gpu_z, int N)    // 核函数。
 {
-    // ============= 二维启用 ================================
-    int glabal_idx = blockDim.x * (blockIdx.y * gridDim.x + blockIdx.x) + threadIdx.x;  // 计算每个线程的唯一索引(二维)。
-    // =====================================================
 
 
-    // ============= 一维启用 =================================
+    // =========================一维=========================
     //int glabal_idx = blockDim.x * blockIdx.x + threadIdx.x;
     // ======================================================
 
 
-    if(glabal_idx < N)  
+    // ======================================二维======================================
+    int global_idx = blockDim.x * (blockIdx.y * gridDim.x + blockIdx.x) + threadIdx.x;    // 计算所有线程的唯一索引。
+    // ================================================================================
+
+    if(global_idx < N)   // global_idx < N是因为main函数中设置的线程数量大于数据数量，防止数组越界。
     {
-        gpu_z[glabal_idx] = gpu_x[glabal_idx] + gpu_y[glabal_idx];   
+        gpu_z[global_idx] = gpu_x[global_idx] + gpu_y[global_idx];    // gpu_z = gpu_x + gpu_y
     }
 
 }
 
 
+
 int main()
 {
-    int N = 10000;  
-    int block_num = 256;  
-    
+    int N = 10000;  // 总共有10000个数
+    int blockSize = 256;   // 假设1个block存在256个线程。
+    dim3 block(blockSize);  // 启动核函数可直接用blockSize，但是用上dim3比较规范。
 
 
-    // =============一维启用================================
+    // =======================================一维=======================================
     // 定义一维grid
-    //int grid_num_one = ceil((N + block_num - 1.) / block_num);   // 此处的1.很细节，避免整数除断。
-    // 解析：(N + block_num - 1.) / block_num，其实就是为了定义足够的block，让线程很好地适配数据的数量。
+    // int gridSize_one = (N + blockSize - 1.) / blockSize;   // 此处的1.很细节，避免整数除断。
+    // 解析：(N + blockSize - 1.) / blockSize，其实就是为了定义足够的block，让线程很好地适配数据的数量。
     // 解析：(10000 + 256 - 1.)/256 ≈ 40.05859 向上取整为41，因此需要41个256大小地block。
     // 解析：41*256 = 10496个线程，但是我们只有10000个数据，因此有496个线程是闲置的。
 
-    //dim3 grid_one(grid_num_one);   // 定义一个1维网格，其实也就是定义了grid_num_one个block，再用grid_num_one * block_num就得到总线程数。
-    // =====================================================
+    //dim3 grid_one(gridSize_one);   // 定义一个1维网格，其实也就是定义了gridSize_one个block，再用gridSize_one * blockSize就得到总线程数。
+    // ==================================================================================
 
 
 
-
-    int grid_num_two = ceil(sqrt((N + block_num - 1.) / block_num)); 
-
+    // =======================================二维=======================================
+    // 定义二维grid
+    int gridSize_two = ceil(sqrt((N + blockSize - 1.) / blockSize));     // 此处的1.很细节，避免整数除断。
+    //解析：sqrt((N + blockSize - 1.) / blockSize)中多了一个sqrt是因为我们定义的是一个n×n的网格，所以我们要开根号，获得x和y的数量，如上sqrt(40.05859)并向上取整为7。
+    // 因此我们定义了一个7×7大小的网格grid，展平成一维就是49个block，总共存在49*256 = 12544个thread，闲置2544个线程，这比一维grid浪费的资源更多。
 
     
-    dim3 grid_two(grid_num_two, grid_num_two);
+    dim3 grid_two(gridSize_two, gridSize_two);
+    // ==================================================================================
 
 
-
+    // 定义cpu_x、cpu_y用于cpu端初始化两个一维张量，cpu_z用于存储cpu端的张量和。
     float *cpu_x, *cpu_y, *cpu_z;  
 
+    // 定义gpu_x、gpu_y用于接收cpu端传到gpu端的张量数据，gpu_z用于在gpu中存储张量和。
     float *gpu_x, *gpu_y, *gpu_z;
 
-
+    // 这里为什么会又设定一个值呢？因为cpu_x对应gpu_x，cpu_y对应gpu_y，cpu_z对应cpu_x + cpu_y，gpu_z对应的gpu_x + gpu_y。
+    // 这时我需要将gpu_z的结果传回cpu端，但是没有对应接收的变量了。因此我需要再定义一个变量用于接收gpu端到cpu端的结果传输。
+    // 其实不定义它也可以，直接用cpu_z来接收结果，这样操作就保存不了cpu端的结果，但可以保存gpu端的输出结果。
+    // 综上所述，代码总是需要重新定义一个变量用于接收cpu或gpu中的值，以便用于后续cpu结果和gpu结果的一个合格性检查。
     float *gpu_result;
 
-    int nbytes = N * sizeof(float);  
+    int nbytes = N * sizeof(float);  // 分配空间所需的总字节数 = 总数据量*数据类型的字节数，因为定义的数据是用的float，因此：N * sizeof(float)。
 
+    // cpu中用C语言中的malloc分配空间。
     cpu_x = (float *)malloc(nbytes);
     cpu_y = (float *)malloc(nbytes);
     cpu_z = (float *)malloc(nbytes);
 
-    gpu_result = (float *)malloc(nbytes);   
+    gpu_result = (float *)malloc(nbytes);   // 它是cpu端接收gpu结果的变量，所以用cpu的C语言中的malloc分配空间，不要搞混淆了。
+
 
 // ===================== 错误检测 ==================================
     gpuErrchk(cudaMalloc((void**)&gpu_x, nbytes));
@@ -129,13 +140,13 @@ int main()
 
      // ============= 一维启用 ================================
     // 启动核函数(一维)。
-    //vector_add<<<grid_one, block_num>>>(gpu_x, gpu_y, gpu_z, N);
+    //vector_add<<<grid_one, block>>>(gpu_x, gpu_y, gpu_z, N);
     // =======================================================
 
 
     // ============= 二维启用 ================================
     // 启动核函数(二维)。
-    vector_add<<<grid_two, block_num >>>(gpu_x, gpu_y, gpu_z, N);
+    vector_add<<<grid_two, block >>>(gpu_x, gpu_y, gpu_z, N);
     // ======================================================
 
 
